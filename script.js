dd00396129f341b139c09fea894004f93c7c7af5
Guille-val <guille.v2018@gmail.com>
2024-08-11 13:52:49 -0300

    Cambios con media query tablet

--- script.js
+++ script.js
@@ -18,17 +18,26 @@ function btnDesencriptar(){
 function btncopiar(){
     navigator.clipboard.writeText(mensaje.value);
     alert("Texto copiado al portapapeles");
+    mensaje.value = '';
+    textArea.value = '';
 }
 
+
 function encriptar(stringEncriptada){
-    stringEncriptada = stringEncriptada.toLowerCase();
-    
-    for(let i=0; i<matrizCodigo.length; i++){
-        if(stringEncriptada.includes(matrizCodigo[i][0])){
-            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+    for (let i = 0; i < stringEncriptada.length; i++) {
+        if (stringEncriptada[i] >= 'A' && stringEncriptada[i] <= 'Z') {
+            return "El texto no debe contener letras mayusculas ni acentos. Intenta de nuevo!";
+        }else {
+            for(let i=0; i<matrizCodigo.length; i++){
+
+                if(stringEncriptada.includes(matrizCodigo[i][0])){
+                    stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+                }
+            }
+            return stringEncriptada;
         }
     }
-    return stringEncriptada;
+    
 }
 
 function desencriptar(stringDesencriptada){
